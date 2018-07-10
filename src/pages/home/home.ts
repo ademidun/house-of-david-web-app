fc150c37cd1403af2522635e36bc2b4bcebfa662
ademidun <tomiademidun@gmail.com>
2018-07-10 19:40:36 -0400

    added zoom animation to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+  zoomTime = false;
   constructor(public navCtrl: NavController) {
-
+  console.log('zoomTime',this.zoomTime);
   }
+
+  ngOnInit () {
+    setTimeout((() => {
+      this.zoomTime=true;
+      console.log('zoomTime',this.zoomTime);
+    }), 200);
+    console.log('zoomTime',this.zoomTime);
+  }
+
 
 }
